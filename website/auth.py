789d4c0b58200dc0df50c46e57481187f5624ba3
ZoroAi <sauravkc456@gmail.com>
2023-01-31 16:51:03 +0000

    form validation

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods= ['GET','POST'])
 def login():
-    return '<p>Login</p>'
+    data = request.form
+    print(data)
+    return render_template('login.html')
+
+
+@auth.route('/sign-up', methods= ['GET','POST'])
+def sign_up():
+    #  Form Validation
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email < 7):
+            flash('Email must be longer than 6 characters', category='err')
+        elif len(firstName<2):
+            flash('First Name must be longer than 1 characters', category='err')
+        elif password1 != password2:
+            flash('Password does not match', category='err')
+        elif len(password1) < 7:
+            flash('Password must be longer than 6 characters', category='err')
+        else:
+            # Add user to Database
+            flash('Account Created', category = 'success')
+            pass
+        
+    return render_template('sign_up.html')
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
-
-@auth.route('/sign-up')
-def sign_up():
-    return '<p>Sign Up</p>'
